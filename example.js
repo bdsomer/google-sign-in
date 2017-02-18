a366e0a8283c36a3d525b4e67878f52c3e666977
Bennett <javacoolme@icloud.com>
2017-02-17 19:19:16 -0800

    Updated example.js
    Removed empty lines on lines 2 and 4. Now the file matches the example
    in the README.md.

--- example.js
+++ example.js
@@ -1,7 +1,5 @@
 var GoogleSignIn = require("./index.js");
-
 var project = new GoogleSignIn.Project(["your-client-id.apps.googleusercontent.com"]);
-
 project.verifyToken("client-id-token", function(err, data) {
 	if (err) {
 		console.log(err); // Logs { description: 'Invalid Value' }
