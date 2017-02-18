423b763833ab3723c7c487d92811045c23a1a798
Bennett <javacoolme@icloud.com>
2017-02-17 23:59:00 -0800

    Updated example, version, readme

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 var GoogleSignIn = require("google-sign-in");
 var project = new GoogleSignIn.Project(["your-client-id.apps.googleusercontent.com"]);
-project.verifyToken("client-id-token", function(err, data) {
+project.verifyToken("token", function(err, data) {
 	if (err) {
 		console.log(err); // Logs { description: 'Invalid Value' }
 	} else {
